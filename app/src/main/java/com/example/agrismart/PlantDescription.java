10e7007f52c9fbb0aca5376a2557599d205f3af0
Tanzimul Hoque <tanzimul.24csedu.047@gmail.com>
2019-09-01 19:17:47 +0600

    Ui files

---
+++ app/src/main/java/com/example/agrismart/PlantDescription.java
@@ -0,0 +1,42 @@
+package com.example.agrismart;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PlantDescription extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView)findViewById(R.id.mytextview);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.potato);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        String myText = "";
+        int in;
+        try {
+            in = inputStream.read();
+            while (in != -1)
+            {
+                byteArrayOutputStream.write(in);
+                in = inputStream.read();
+            }
+            inputStream.close();
+
+            myText = byteArrayOutputStream.toString();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        myTextView.setText(myText);
+    }
+
+}
---
+++ app/src/main/java/com/example/agrismart/SelectionPage.java
@@ -0,0 +1,15 @@
+package com.example.agrismart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selection_page);
+    }
+}
