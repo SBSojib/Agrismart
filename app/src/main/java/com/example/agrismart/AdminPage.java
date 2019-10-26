5de354f120364eaf8190af3719104e4332acdc33
Sojib <sojib.24csedu.037@gmail.com>
2019-10-27 02:33:06 +0600

    Adding required attributes to Crop2

--- app/src/main/java/com/example/agrismart/AdminPage.java
+++ app/src/main/java/com/example/agrismart/AdminPage.java
@@ -14,14 +14,31 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminPage extends AppCompatActivity {
     DatabaseReference mDatabase;
+
+    EditText et1 = null;
+    EditText et2 = null;
+    EditText et3 = null;
+    EditText et4 = null;
+    EditText minTemp = null;
+    EditText maxTemp = null;
+    EditText minHumidity = null;
+    EditText maxHimidity = null;
+    EditText fertilizingInterval = null;
+    EditText totalFertilizingTime = null;
+    EditText insecticideInterval = null;
+    EditText totalInsecticideTime = null;
+    EditText cultivationDuration = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
-        final EditText et1= (EditText)findViewById(R.id.crpName);
-        final EditText et2= (EditText)findViewById(R.id.crpLat);
-        final EditText et3= (EditText)findViewById(R.id.crpLon);
-        final EditText et4= (EditText)findViewById(R.id.crpMon);
+        et1= (EditText)findViewById(R.id.crpName);
+        et2= (EditText)findViewById(R.id.crpLat);
+        et3= (EditText)findViewById(R.id.crpLon);
+        et4= (EditText)findViewById(R.id.crpMon);
 
 
         Button btn = (Button)findViewById(R.id.addcrop);
